b020cc4e1f1905c477581831b8d2d1954c3a7013
Pasindu Nuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2025-01-04 23:50:42 +0530

    feat: :sparkles: Enhance navigation by adding Tabs screen and updating stack navigator

--- student_care/App.js
+++ student_care/App.js
@@ -1,18 +1,35 @@
 import 'react-native-gesture-handler';
-import * as React from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Login from './components/Login';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import StudentDashboard from './components/StudentDashboard';
+import Login from './components/Login';
+import Tabs from './components/Tabs';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name='Dash' component={StudentDashboard}/>
-      </Stack.Navigator>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Tabs" 
+            component={Tabs}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Dashboard"
+            component={StudentDashboard}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaView>
   );
@@ -21,8 +38,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
